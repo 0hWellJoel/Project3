3f1c39b7a51096aca8e865ead15c4d133d2c2a01
inafis <kbrown87@its.jnj.com>
2020-08-12 20:12:31 -0400

    init

---
+++ routes/users.js
@@ -0,0 +1,50 @@
+var db = require("../models");
+var passport = require("../passport");
+var router = requre('express').Router();
+
+router.post("/api/login", passport.authenticate("local"), function(req, res) {
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      id: req.user.id
+    });
+  });
+
+// Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+// how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+// otherwise send back an error
+router.post("/api/signup", function(req, res) {
+db.User.create({
+    email: req.body.email,
+    password: req.body.password
+})
+    .then(function() {
+    res.redirect(307, "/api/login");
+    })
+    .catch(function(err) {
+    res.status(401).json(err);
+    });
+});
+
+// Route for logging user out
+router.get("/logout", function(req, res) {
+req.logout();
+res.redirect("/");
+});
+
+// Route for getting some data about our user to be used client side
+router.get("/api/user_data", function(req, res) {
+if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.json({});
+} else {
+    // Otherwise send back the user's email and id
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+    email: req.user.email,
+    id: req.user.id
+    });
+}
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
+var session = require("express-session");
+
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require('./routes');
+var passport = require("./config/passport");
 
+var db = require("./models");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -14,6 +28,8 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
 });
